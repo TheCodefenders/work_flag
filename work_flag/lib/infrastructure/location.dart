e1ff0585bbcbdb03837422894e05fb5444ac99ff
Renan Winter Spatin <rwspatin@gmail.com>
2020-08-22 22:09:52 -0300

    Create a localization logic when save and update

---
+++ work_flag/lib/infrastructure/location.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+  Position position;
+
+  getLocation() {
+    geolocator
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((value) => position = value);
+  }
+
+  Future<String> getAddressFromLatLgn() async {
+    try {
+      getLocation();
+      var p = await geolocator.placemarkFromCoordinates(
+        position.latitude,
+        position.longitude,
+      );
+
+      debugPrint("Second local - ${position?.latitude.toString()}");
+
+      var place = p[0];
+
+      return "${place.locality}, ${place.postalCode}, ${place.country}";
+    } catch (e) {
+      //debugPrint(e);
+      return "";
+    }
+  }
+
+  Future<bool> IsSameLocation(String locationDb) async {
+    var nowLocation = await getAddressFromLatLgn();
+
+    return nowLocation == locationDb;
+  }
+}
