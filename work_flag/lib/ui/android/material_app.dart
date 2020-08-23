844ebeb4d1538b3c7abc31da928ec1ca021c9179
Renan Winter Spatin <rwspatin@gmail.com>
2020-08-22 22:18:04 -0300

    set optional darktheme

--- work_flag/lib/ui/android/material_app.dart
+++ work_flag/lib/ui/android/material_app.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
 
-class MyMaterialApp extends StatelessWidget {
+class MyMaterialApp extends StatefulWidget {
+  final bool isDarkMode;
+
+  const MyMaterialApp({Key key, this.isDarkMode}) : super(key: key);
+
+  @override
+  _MyMaterialAppState createState() => _MyMaterialAppState();
+}
+
+class _MyMaterialAppState extends State<MyMaterialApp> {
   var name = 'Marcador Horas';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: name,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: widget.isDarkMode
+          ? ThemeData.dark()
+          : ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
       home: HomePage(
         title: name,
       ),
